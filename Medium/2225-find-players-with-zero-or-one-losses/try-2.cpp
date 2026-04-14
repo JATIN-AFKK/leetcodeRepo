8cf4f9149d71d1783370a8feaf24b6f92859adc2
JATIN-AFKK <jg9257694@gmail.com>
2026-04-14 17:28:21 +0530

    Add Medium/2225-find-players-with-zero-or-one-losses/try-2.cpp

---
+++ Medium/2225-find-players-with-zero-or-one-losses/try-2.cpp
@@ -0,0 +1,33 @@
+/*
+ * Problem #2225: Find Players With Zero or One Losses
+ * Difficulty: Medium
+ * Submission: Try 2
+ * status: Accepted
+ * Language: cpp
+ * Date: 13/02/2026, 10:54:31
+ * Link: https://leetcode.com/problems/find-players-with-zero-or-one-losses/
+ */
+
+class Solution {
+public:
+    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+        map<int,int> wp, lp;
+        vector<vector<int>> ans(2);
+        for(vector<int> wl : matches){
+            int w = wl[0], l = wl[1];
+            wp[w]++;
+            lp[l]++;
+        }
+
+        for(auto [player , win] : wp){
+            if(lp[player] != 0) continue;
+            ans[0].push_back(player);
+        }
+        for(auto [player , lose] : lp){
+            if(lose != 1) continue;
+            ans[1].push_back(player);
+        }
+
+        return ans;
+    }
+};
