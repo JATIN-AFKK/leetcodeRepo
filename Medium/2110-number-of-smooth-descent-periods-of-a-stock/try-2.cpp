4b30e2ad0c38c3ca338ce9fe3dde11d6a2f7b620
JATIN-AFKK <jg9257694@gmail.com>
2026-04-14 17:11:39 +0530

    Add Medium/2110-number-of-smooth-descent-periods-of-a-stock/try-2.cpp

---
+++ Medium/2110-number-of-smooth-descent-periods-of-a-stock/try-2.cpp
@@ -0,0 +1,22 @@
+/*
+ * Problem #2110: Number of Smooth Descent Periods of a Stock
+ * Difficulty: Medium
+ * Submission: Try 2
+ * status: Accepted
+ * Language: cpp
+ * Date: 04/03/2026, 20:08:01
+ * Link: https://leetcode.com/problems/number-of-smooth-descent-periods-of-a-stock/
+ */
+
+class Solution {
+public:
+    long long getDescentPeriods(vector<int>& prices) {
+        long long ans = 1;
+        int l = 0;
+        for(int e = 1;e < prices.size(); e++){
+            if(prices[e - 1] - prices[e] != 1) l = e;
+            ans += (e - l + 1);
+        }
+        return ans;
+    }
+};
