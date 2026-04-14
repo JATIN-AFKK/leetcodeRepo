1fb2a229573f82c92926a46a4b411728227275c5
JATIN-AFKK <jg9257694@gmail.com>
2026-04-14 17:15:50 +0530

    Add Medium/3035-maximum-palindromes-after-operations/try-3.cpp

---
+++ Medium/3035-maximum-palindromes-after-operations/try-3.cpp
@@ -0,0 +1,46 @@
+/*
+ * Problem #3035: Maximum Palindromes After Operations
+ * Difficulty: Medium
+ * Submission: Try 3
+ * status: Accepted
+ * Language: cpp
+ * Date: 15/02/2026, 21:34:26
+ * Link: https://leetcode.com/problems/maximum-palindromes-after-operations/
+ */
+
+class Solution {
+public:
+    int maxPalindromesAfterOperations(vector<string>& words) {
+        vector<int> count(26, 0);
+        for(string str : words){
+            for(char ch : str){
+                count[ch - 'a']++;
+            }
+        }
+        int even = 0, odd = 0;
+        for(int i = 0;i < 26;i++){
+            if(count[i]%2 == 1) odd++;
+            even += count[i]/2;
+        }
+
+        int ans = 0;
+        vector<int> len;
+        for(int i = 0;i < words.size();i++) len.push_back(words[i].size());
+
+        sort(len.begin(), len.end());
+        for(int l : len){
+            if(l%2 == 1){
+                if(odd > 0) odd--;
+                else {
+                    even--;
+                    odd++;
+                }
+            }
+            int need = l/2;
+            if(need > even) break;
+            even -= need;
+            ans++;
+        }
+        return ans;
+    }
+};
