c1ed73b0e565171f283813be5459ad4a1431764f
JATIN-AFKK <jg9257694@gmail.com>
2026-04-14 17:30:03 +0530

    Add Medium/1419-minimum-number-of-frogs-croaking/try-2.cpp

---
+++ Medium/1419-minimum-number-of-frogs-croaking/try-2.cpp
@@ -0,0 +1,43 @@
+/*
+ * Problem #1419: Minimum Number of Frogs Croaking
+ * Difficulty: Medium
+ * Submission: Try 2
+ * status: Accepted
+ * Language: cpp
+ * Date: 09/02/2026, 17:52:11
+ * Link: https://leetcode.com/problems/minimum-number-of-frogs-croaking/
+ */
+
+class Solution {
+public:
+    bool isEqual(vector<int> vec){
+        for(int i = 1;i < vec.size();i++) if(vec[i] != vec[i-1]) return false;
+        return true;
+    }
+    int minNumberOfFrogs(string croakOfFrogs) {
+        unordered_map<char, int> up = {
+            {'c', 0},
+            {'r', 1},
+            {'o', 2},
+            {'a', 3},
+            {'k', 4}
+        };                                                                
+        vector<int> count(5, 0);
+
+        int ans = 0;
+        for(char ch : croakOfFrogs){
+            if(up.find(ch) == up.end()) return -1;
+            else count[up[ch]]++;
+
+            if(up[ch] > 0 && count[up[ch]] > count[up[ch] - 1]) return -1;
+            ans = max(ans, count[0] - count[4]);
+        }
+        if(isEqual(count))
+        return ans;
+        return -1;
+    }
+};
+
+// 1 0 0 0 0
+// 1 1 1 1 1  ans = 1;
+// 2 2 2 2 2  ans = 2-1 = 1
