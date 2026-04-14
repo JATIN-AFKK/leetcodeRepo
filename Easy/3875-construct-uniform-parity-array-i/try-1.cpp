68981d00a1ae622c5c60c0b391cd7f181b4987d7
JATIN-AFKK <jg9257694@gmail.com>
2026-04-14 17:05:02 +0530

    Add Easy/3875-construct-uniform-parity-array-i/try-1.cpp

---
+++ Easy/3875-construct-uniform-parity-array-i/try-1.cpp
@@ -0,0 +1,16 @@
+/*
+ * Problem #3875: Construct Uniform Parity Array I
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 22/03/2026, 08:03:16
+ * Link: https://leetcode.com/problems/construct-uniform-parity-array-i/
+ */
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
