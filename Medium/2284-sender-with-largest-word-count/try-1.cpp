7868e320bd63adcf4106f18dc40e02b145b694cb
JATIN-AFKK <jg9257694@gmail.com>
2026-04-14 17:16:58 +0530

    Add Medium/2284-sender-with-largest-word-count/try-1.cpp

---
+++ Medium/2284-sender-with-largest-word-count/try-1.cpp
@@ -0,0 +1,43 @@
+/*
+ * Problem #2284: Sender With Largest Word Count
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 13/02/2026, 13:17:49
+ * Link: https://leetcode.com/problems/sender-with-largest-word-count/
+ */
+
+class Solution {
+public:
+    string largestWordCount(vector<string>& messages, vector<string>& senders) {
+        unordered_map<string , int> up;
+
+        int n = messages.size();
+        for(int i = 0;i < n;i++){
+            string message = messages[i], word;
+            string sender = senders[i];
+
+            stringstream ss(message);
+            int count = 0;
+
+            while(ss >> word) count++;
+            up[sender] += count;
+        }
+
+        string ans = "";
+        int f = 0;
+
+        for(auto [sender, count] : up){
+            if(f == 0 || count > f ){
+                ans = sender;
+                f = count;
+            }
+            else if(f == count){
+                ans = max(ans, sender);
+            }
+
+        }
+        return ans;
+    }
+};
