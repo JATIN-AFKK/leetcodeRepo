93fa0d5c3e87db6807ffcdd05a2058d287602419
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 21:03:36 +0530

    Add Easy/217-contains-duplicate/try-11.cpp

---
+++ Easy/217-contains-duplicate/try-11.cpp
@@ -0,0 +1,22 @@
+/*
+ * Problem #217: Contains Duplicate
+ * Difficulty: Easy
+ * Submission: Try 11
+ * status: Accepted
+ * Language: cpp
+ * Date: 04/12/2024, 16:09:22
+ * Link: https://leetcode.com/problems/contains-duplicate/
+ */
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> map;
+        for(int i=0;i<nums.size();i++){
+            if (++map[nums[i]]==2){
+                return true;
+            }
+        }
+        return false;
+    }
+};
