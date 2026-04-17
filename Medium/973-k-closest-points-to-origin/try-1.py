f118428edfe455afefd44a2a79bdfc27bb3c905a
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 20:16:20 +0530

    Add Medium/973-k-closest-points-to-origin/try-1.py

---
+++ Medium/973-k-closest-points-to-origin/try-1.py
@@ -0,0 +1,16 @@
+/*
+ * Problem #973: K Closest Points to Origin
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: python3
+ * Date: 17/12/2024, 11:44:44
+ * Link: https://leetcode.com/problems/k-closest-points-to-origin/
+ */
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        distance = [ [p[0]**2 + p[1]**2 , ind ] for ind , p in enumerate(points)]
+        heapify(distance)
+        return [points[heappop(distance)[-1]] for _ in range(k)]
+        
