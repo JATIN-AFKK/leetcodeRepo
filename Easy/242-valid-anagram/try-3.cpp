c7d25060abfe78bfe2aa5318831acb254fc22276
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 21:06:18 +0530

    Add Easy/242-valid-anagram/try-3.cpp

---
+++ Easy/242-valid-anagram/try-3.cpp
@@ -0,0 +1,34 @@
+/*
+ * Problem #242: Valid Anagram
+ * Difficulty: Easy
+ * Submission: Try 3
+ * status: Accepted
+ * Language: cpp
+ * Date: 22/11/2024, 00:24:17
+ * Link: https://leetcode.com/problems/valid-anagram/
+ */
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()){
+            return false;
+        }
+        unordered_map<char,int> map;
+        for(char i : s){
+            map[i]++;
+        }
+        for(char i : t){
+            if(map[i]==1){
+                map.erase(i);
+                continue;
+            }
+            else if(!map.contains(i)){
+                return false;
+            }
+            map[i]--;
+        }
+        return map.size()==0;
+    }
+        
+};
