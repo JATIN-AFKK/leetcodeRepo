a0893161ce7acf48375d0a970d1ae07e8c13e54b
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 20:58:49 +0530

    Add Medium/3-longest-substring-without-repeating-characters/try-8.py

---
+++ Medium/3-longest-substring-without-repeating-characters/try-8.py
@@ -0,0 +1,26 @@
+/*
+ * Problem #3: Longest Substring Without Repeating Characters
+ * Difficulty: Medium
+ * Submission: Try 8
+ * status: Accepted
+ * Language: python3
+ * Date: 07/12/2024, 22:22:55
+ * Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ */
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        map=Counter()
+        end=0
+        intial=0
+        maximum=0
+        while(end<len(s)):
+            map[s[end]]+=1
+            while(map[s[end]]>1):
+                map[s[intial]]-=1
+                intial+=1
+            if(maximum<(end-intial+1)):
+                maximum=end-intial+1
+            end+=1
+        return maximum
+        
