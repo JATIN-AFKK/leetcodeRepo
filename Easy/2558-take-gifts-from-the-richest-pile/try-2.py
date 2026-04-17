11f458bcf8b1aca61aa9a56a1157e0b0f1ee2bc4
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 20:55:23 +0530

    Add Easy/2558-take-gifts-from-the-richest-pile/try-2.py

---
+++ Easy/2558-take-gifts-from-the-richest-pile/try-2.py
@@ -0,0 +1,18 @@
+/*
+ * Problem #2558: Take Gifts From the Richest Pile
+ * Difficulty: Easy
+ * Submission: Try 2
+ * status: Accepted
+ * Language: python3
+ * Date: 12/12/2024, 22:22:37
+ * Link: https://leetcode.com/problems/take-gifts-from-the-richest-pile/
+ */
+
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        for i in range(0,k):
+            gifts.sort()
+            gifts[-1]=floor(sqrt(gifts[-1]))
+        
+        return sum(gifts[:])
+
