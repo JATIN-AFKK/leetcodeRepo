a09aaa6b0cb917c135a95631e7b00f62706c0aa9
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 20:57:35 +0530

    Add Medium/424-longest-repeating-character-replacement/try-7.py

---
+++ Medium/424-longest-repeating-character-replacement/try-7.py
@@ -0,0 +1,29 @@
+/*
+ * Problem #424: Longest Repeating Character Replacement
+ * Difficulty: Medium
+ * Submission: Try 7
+ * status: Accepted
+ * Language: python3
+ * Date: 08/12/2024, 16:59:40
+ * Link: https://leetcode.com/problems/longest-repeating-character-replacement/
+ */
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        d={}
+        mx=0
+        start=0
+        re=0
+        for end in range(len(s)):
+            if s[end] in d:
+                d[s[end]]+=1
+            else:
+                d[s[end]]=1
+            mx=max(mx,d[s[end]])
+            while(end-start+1-mx>k):
+                d[s[start]]-=1
+                start+=1
+            
+            re=max(re,end-start+1)
+        return re
+        
