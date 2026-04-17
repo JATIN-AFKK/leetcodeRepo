d26132f7841a836109beae2faaa393633c45ffc6
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 21:01:40 +0530

    Add Easy/242-valid-anagram/try-15.py

---
+++ Easy/242-valid-anagram/try-15.py
@@ -0,0 +1,29 @@
+/*
+ * Problem #242: Valid Anagram
+ * Difficulty: Easy
+ * Submission: Try 15
+ * status: Accepted
+ * Language: python3
+ * Date: 06/12/2024, 15:59:58
+ * Link: https://leetcode.com/problems/valid-anagram/
+ */
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        d1={}
+        d2={}
+        for i in s:
+            if i not in d1:
+                d1[i]=0
+            else:
+                d1[i]+=1
+        
+        for i in t:
+            if i not in d2:
+                d2[i]=0
+            else:
+                d2[i]+=1
+        
+        return d1==d2
+        
+        
