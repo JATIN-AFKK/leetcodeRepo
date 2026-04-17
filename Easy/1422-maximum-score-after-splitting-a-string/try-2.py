901c0a0c46acee62e63dc36aa27e07bdc265e7e4
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 20:14:07 +0530

    Add Easy/1422-maximum-score-after-splitting-a-string/try-2.py

---
+++ Easy/1422-maximum-score-after-splitting-a-string/try-2.py
@@ -0,0 +1,30 @@
+/*
+ * Problem #1422: Maximum Score After Splitting a String
+ * Difficulty: Easy
+ * Submission: Try 2
+ * status: Accepted
+ * Language: python3
+ * Date: 01/01/2025, 16:17:09
+ * Link: https://leetcode.com/problems/maximum-score-after-splitting-a-string/
+ */
+
+class Solution:
+    def maxScore(self, s: str) -> int:
+        n = len(s)
+        r_sum = 0
+        for i in range(1, n):
+            if s[i]== '1':
+                r_sum+=1
+
+        l_sum = 1 if s[0] == '0' else 0
+        m = r_sum + l_sum
+        for i in range(1,n-1):
+            if s[i]=='1':
+                r_sum-=1
+            else:
+                l_sum+=1
+            m = max(m , r_sum + l_sum)
+
+        
+        return m
+        
