d30fe26551bc2e05463121136635a87f24763b41
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 20:13:27 +0530

    Add Medium/1985-find-the-kth-largest-integer-in-the-array/try-3.py

---
+++ Medium/1985-find-the-kth-largest-integer-in-the-array/try-3.py
@@ -0,0 +1,17 @@
+/*
+ * Problem #1985: Find the Kth Largest Integer in the Array
+ * Difficulty: Medium
+ * Submission: Try 3
+ * status: Accepted
+ * Language: python3
+ * Date: 01/01/2025, 18:52:20
+ * Link: https://leetcode.com/problems/find-the-kth-largest-integer-in-the-array/
+ */
+
+class Solution:
+    def kthLargestNumber(self, nums: List[str], k: int) -> str:
+        l1 = [-int(x) for x in nums]
+        heapify(l1)
+        for _ in range(k-1):
+            heappop(l1)
+        return str(-heappop(l1))
