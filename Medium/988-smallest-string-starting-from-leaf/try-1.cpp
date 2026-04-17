17cb85928382af1db9aa598d339714b9f980890e
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 19:50:33 +0530

    Add Medium/988-smallest-string-starting-from-leaf/try-1.cpp

---
+++ Medium/988-smallest-string-starting-from-leaf/try-1.cpp
@@ -0,0 +1,40 @@
+/*
+ * Problem #988: Smallest String Starting From Leaf
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 24/11/2025, 17:36:24
+ * Link: https://leetcode.com/problems/smallest-string-starting-from-leaf/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    string smallestFromLeaf(TreeNode* root) {
+        return dfs(root,"");
+    }
+
+    string dfs(TreeNode* root,string state){
+        if(!root) return "{";
+        string nextState = (char)('a' + root->val) +  state;
+
+        if(!root->left && !root->right) return nextState;
+
+        string left = dfs(root -> left,nextState);
+        string right = dfs(root -> right,nextState);
+
+        if(left > right) return right;
+        else return left;
+    }
+};
