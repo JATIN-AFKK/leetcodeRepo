6fc921ebf8185e2114e4beac0a054578b0172edb
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 21:01:58 +0530

    Add Easy/242-valid-anagram/try-10.cpp

---
+++ Easy/242-valid-anagram/try-10.cpp
@@ -0,0 +1,26 @@
+/*
+ * Problem #242: Valid Anagram
+ * Difficulty: Easy
+ * Submission: Try 10
+ * status: Accepted
+ * Language: cpp
+ * Date: 06/12/2024, 15:49:27
+ * Link: https://leetcode.com/problems/valid-anagram/
+ */
+
+#include<algorithm>
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        if(s.size()!=t.size()){
+            return false;
+        }
+        
+        sort(t.begin(),t.end());
+        sort(s.begin(),s.end());
+        return s==t;    
+    }
+        
+};
