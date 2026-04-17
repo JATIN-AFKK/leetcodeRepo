aea4a9e796f4152d02b91597df5584730443c149
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 20:57:50 +0530

    Add Medium/424-longest-repeating-character-replacement/try-3.cpp

---
+++ Medium/424-longest-repeating-character-replacement/try-3.cpp
@@ -0,0 +1,27 @@
+/*
+ * Problem #424: Longest Repeating Character Replacement
+ * Difficulty: Medium
+ * Submission: Try 3
+ * status: Accepted
+ * Language: cpp
+ * Date: 08/12/2024, 16:53:25
+ * Link: https://leetcode.com/problems/longest-repeating-character-replacement/
+ */
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_map<char,int> map;
+        int in,mx=0,re=0,start=0;
+        for(int end=0;end<s.size();end++){
+            map[s[end]]++;
+            mx=max(mx,map[s[end]]);
+            while(end-start-mx+1>k){
+                map[s[start]]--;
+                start++;
+            }   
+            re=max(re,end-start+1);
+        }
+        return re;
+    }
+};
