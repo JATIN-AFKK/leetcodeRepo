34a3d8f550cc3379b64e63d41c57641c75a15cf8
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 20:14:41 +0530

    Add Medium/739-daily-temperatures/try-5.py

---
+++ Medium/739-daily-temperatures/try-5.py
@@ -0,0 +1,22 @@
+/*
+ * Problem #739: Daily Temperatures
+ * Difficulty: Medium
+ * Submission: Try 5
+ * status: Accepted
+ * Language: python3
+ * Date: 22/12/2024, 22:23:40
+ * Link: https://leetcode.com/problems/daily-temperatures/
+ */
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        re = []
+        n = len(temperatures)
+        for i in range(n-1,-1,-1):
+            while stack and temperatures[stack[-1]]<=temperatures[i] :
+                stack.pop()
+            re.append(stack[-1] - i if stack else 0)
+            stack.append(i)
+        
+        return re[::-1]
