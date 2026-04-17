b4ad2dff05eaac080d99e0c0262f81580fd3f75b
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 20:59:10 +0530

    Add Easy/643-maximum-average-subarray-i/try-9.py

---
+++ Easy/643-maximum-average-subarray-i/try-9.py
@@ -0,0 +1,21 @@
+/*
+ * Problem #643: Maximum Average Subarray I
+ * Difficulty: Easy
+ * Submission: Try 9
+ * status: Accepted
+ * Language: python3
+ * Date: 07/12/2024, 12:36:45
+ * Link: https://leetcode.com/problems/maximum-average-subarray-i/
+ */
+
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        window=0
+        for i in range(k):
+            window+=nums[i]
+
+        mx=window
+        for i in range(k,len(nums)):
+            mx=max(mx,window:=(window+nums[i]-nums[i-k]))
+        return mx/k
+
