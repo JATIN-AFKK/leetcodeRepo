cd556c8789c06362044279f4aacd2d465ff697f0
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 20:56:05 +0530

    Add Medium/12-integer-to-roman/try-11.cpp

---
+++ Medium/12-integer-to-roman/try-11.cpp
@@ -0,0 +1,32 @@
+/*
+ * Problem #12: Integer to Roman
+ * Difficulty: Medium
+ * Submission: Try 11
+ * status: Accepted
+ * Language: cpp
+ * Date: 10/12/2024, 21:08:43
+ * Link: https://leetcode.com/problems/integer-to-roman/
+ */
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<pair<int, string>> d{
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"},
+            {90, "XC"},  {50, "L"},   {40, "XL"}, {10, "X"},   {9, "IX"},
+            {5, "V"},    {4, "IV"},   {1, "I"}};
+            
+        string rom = "";
+        for (auto [value, numeral] : d) {
+            if (num==0){
+                break;
+            }
+            while (num >= value) {
+                num -= value;    
+                rom += numeral;  
+            }
+        }
+        
+        return rom;
+    }
+};
