215b86e97f170cac99edfa56164a27967d276e14
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 20:16:29 +0530

    Add Medium/378-kth-smallest-element-in-a-sorted-matrix/try-6.py

---
+++ Medium/378-kth-smallest-element-in-a-sorted-matrix/try-6.py
@@ -0,0 +1,21 @@
+/*
+ * Problem #378: Kth Smallest Element in a Sorted Matrix
+ * Difficulty: Medium
+ * Submission: Try 6
+ * status: Accepted
+ * Language: python3
+ * Date: 16/12/2024, 19:54:14
+ * Link: https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
+ */
+
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        l1=[]
+        for nest in matrix:
+            l1.extend(nest)
+        heapify(l1)
+        for i in range(k-1):
+            heappop(l1)
+        return heappop(l1)
+        
+
