1ab50f3c1e78353c919185c7f24b8983635936e2
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 21:00:16 +0530

    Add Easy/387-first-unique-character-in-a-string/try-11.py

---
+++ Easy/387-first-unique-character-in-a-string/try-11.py
@@ -0,0 +1,26 @@
+/*
+ * Problem #387: First Unique Character in a String
+ * Difficulty: Easy
+ * Submission: Try 11
+ * status: Accepted
+ * Language: python3
+ * Date: 06/12/2024, 23:20:05
+ * Link: https://leetcode.com/problems/first-unique-character-in-a-string/
+ */
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        d={}
+        for i in range(len(s)):
+            if s[i] not in d:
+                d[s[i]]=[i,1]
+            else:
+                d[s[i]][1]+=1
+        
+        for i in d:
+            if d[i][1]==1:
+                return d[i][0]
+        
+        return -1           
+        
+        
