81bec2554a3f9e782e2f85749efbd4df73522b9e
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 20:04:34 +0530

    Add Medium/3169-count-days-without-meetings/try-2.cpp

---
+++ Medium/3169-count-days-without-meetings/try-2.cpp
@@ -0,0 +1,35 @@
+/*
+ * Problem #3169: Count Days Without Meetings
+ * Difficulty: Medium
+ * Submission: Try 2
+ * status: Accepted
+ * Language: cpp
+ * Date: 24/03/2025, 17:25:45
+ * Link: https://leetcode.com/problems/count-days-without-meetings/
+ */
+
+class Solution {
+public:
+    int countDays(int days, vector<vector<int>>& arr) {
+        sort(arr.begin(),arr.end());
+        int i = 0,count = 0,low = arr[0][0],high = arr[0][1];
+        for(i = 1;i < arr.size();i++){
+            if(arr[i][0] > high){
+                count = count + arr[i][0] - high - 1;
+                high = arr[i][1];
+            }
+            else{
+                if(arr[i][1]>high){
+                    high = arr[i][1];
+                }
+            }
+        }
+        if(high<days){
+            count += days - high;
+        }
+        if(low!=1){
+            count += low - 1;
+        }
+        return count;
+    }
+};
