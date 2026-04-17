889a8abad3fdd4bd722b1f5cdcb778eb1c7c6309
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 21:04:33 +0530

    Add Easy/121-best-time-to-buy-and-sell-stock/try-7.py

---
+++ Easy/121-best-time-to-buy-and-sell-stock/try-7.py
@@ -0,0 +1,26 @@
+/*
+ * Problem #121: Best Time to Buy and Sell Stock
+ * Difficulty: Easy
+ * Submission: Try 7
+ * status: Accepted
+ * Language: python3
+ * Date: 02/12/2024, 18:47:04
+ * Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+ */
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        m = 0  
+        s = 0  
+        for i in range(1, len(prices)):
+            diff = prices[i] - prices[i - 1] 
+            s += diff  
+            if s < 0:   
+                s = 0
+            elif s > m: 
+                m = s
+        return m
+
+            
+
+        
