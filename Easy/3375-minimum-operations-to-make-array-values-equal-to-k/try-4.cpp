b8b1594ba03090366185b0255b541d6dc3fe9646
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 20:58:10 +0530

    Add Easy/3375-minimum-operations-to-make-array-values-equal-to-k/try-4.cpp

---
+++ Easy/3375-minimum-operations-to-make-array-values-equal-to-k/try-4.cpp
@@ -0,0 +1,31 @@
+/*
+ * Problem #3375: Minimum Operations to Make Array Values Equal to K
+ * Difficulty: Easy
+ * Submission: Try 4
+ * status: Accepted
+ * Language: cpp
+ * Date: 08/12/2024, 12:04:59
+ * Link: https://leetcode.com/problems/minimum-operations-to-make-array-values-equal-to-k/
+ */
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        unordered_set<int> uni(nums.begin(),nums.end());
+        int mi=INT_MAX;
+
+        for(auto i : uni){
+            mi=min(mi,i);
+        }
+
+        if(mi<k){
+            return -1;
+        }
+        else if(mi==k){
+            return uni.size()-1;
+        }
+        else{
+            return uni.size();
+        }
+    }
+};
