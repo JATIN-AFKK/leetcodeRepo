c28874f3bab390484af8320b8ee4c87fb41336f0
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 21:00:41 +0530

    Add Easy/387-first-unique-character-in-a-string/try-4.cpp

---
+++ Easy/387-first-unique-character-in-a-string/try-4.cpp
@@ -0,0 +1,28 @@
+/*
+ * Problem #387: First Unique Character in a String
+ * Difficulty: Easy
+ * Submission: Try 4
+ * status: Accepted
+ * Language: cpp
+ * Date: 06/12/2024, 22:28:31
+ * Link: https://leetcode.com/problems/first-unique-character-in-a-string/
+ */
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,int> d,e;
+        for(int i=0;i<s.size();i++){
+            if(d[s[i]]==0){
+                e[s[i]]=i;
+            }
+            d[s[i]]++;
+        }
+        for(auto i : s){
+            if(d[i]==1){
+                return e[i];
+            }
+        }
+        return -1;
+    }
+};
