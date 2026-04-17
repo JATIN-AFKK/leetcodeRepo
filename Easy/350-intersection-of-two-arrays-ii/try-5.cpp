9639b8fc27ec8bcb6248ab2652bd2524cba554ce
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 21:02:48 +0530

    Add Easy/350-intersection-of-two-arrays-ii/try-5.cpp

---
+++ Easy/350-intersection-of-two-arrays-ii/try-5.cpp
@@ -0,0 +1,28 @@
+/*
+ * Problem #350: Intersection of Two Arrays II
+ * Difficulty: Easy
+ * Submission: Try 5
+ * status: Accepted
+ * Language: cpp
+ * Date: 04/12/2024, 21:52:38
+ * Link: https://leetcode.com/problems/intersection-of-two-arrays-ii/
+ */
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> map1;
+        vector<int> vec;
+
+        for(int i :nums1){
+            map1[i]++;
+        }
+        for(int i : nums2){
+            if(map1[i]>0){
+                vec.push_back(i);
+                map1[i]--;
+            }
+        }
+        return vec;
+    }
+};
