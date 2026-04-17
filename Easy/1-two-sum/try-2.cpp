54560646a33d97a8f8fc416f8b837e7b48805972
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 21:07:55 +0530

    Add Easy/1-two-sum/try-2.cpp

---
+++ Easy/1-two-sum/try-2.cpp
@@ -0,0 +1,26 @@
+/*
+ * Problem #1: Two Sum
+ * Difficulty: Easy
+ * Submission: Try 2
+ * status: Accepted
+ * Language: cpp
+ * Date: 27/09/2024, 14:00:37
+ * Link: https://leetcode.com/problems/two-sum/
+ */
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i,j,flag=0;
+        vector<int> n;
+        for(i=0;i<nums.size();i++){
+            for(j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                   
+                   return {i,j};
+                }
+            }
+        }
+        return {};     
+    }
+};
