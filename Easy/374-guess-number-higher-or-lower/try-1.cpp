40100b9323a113eb082bcf157c17c42cdad821c7
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 20:11:12 +0530

    Add Easy/374-guess-number-higher-or-lower/try-1.cpp

---
+++ Easy/374-guess-number-higher-or-lower/try-1.cpp
@@ -0,0 +1,38 @@
+/*
+ * Problem #374: Guess Number Higher or Lower
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 16/01/2025, 13:11:24
+ * Link: https://leetcode.com/problems/guess-number-higher-or-lower/
+ */
+
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        long long int i = 1, j = n,mid;
+        while(i<=j){
+            mid = i + (j - i)/2;
+            if(guess(mid) == 0){
+                return mid;
+            }
+            else if(guess(mid) == 1){
+                i = mid + 1;
+            }
+            else{
+                j = mid - 1;
+            }
+        }
+        return mid;
+    }
+};
