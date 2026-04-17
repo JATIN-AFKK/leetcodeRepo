b9a33e7fc2358aa6ba48efd85f72029386013243
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 20:01:44 +0530

    Add Easy/3658-gcd-of-odd-and-even-sums/try-1.cpp

---
+++ Easy/3658-gcd-of-odd-and-even-sums/try-1.cpp
@@ -0,0 +1,16 @@
+/*
+ * Problem #3658: GCD of Odd and Even Sums
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 25/08/2025, 22:21:28
+ * Link: https://leetcode.com/problems/gcd-of-odd-and-even-sums/
+ */
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
