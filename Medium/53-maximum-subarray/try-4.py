adf2744cb7182bd4e704cc876d1d435864e4b002
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 21:14:34 +0530

    Add Medium/53-maximum-subarray/try-4.py

---
+++ Medium/53-maximum-subarray/try-4.py
@@ -0,0 +1,23 @@
+/*
+ * Problem #53: Maximum Subarray
+ * Difficulty: Medium
+ * Submission: Try 4
+ * status: Accepted
+ * Language: python3
+ * Date: 13/12/2024, 15:20:06
+ * Link: https://leetcode.com/problems/maximum-subarray/
+ */
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        mx=-float('inf')
+        s=0
+        for i in nums:
+            s+=i
+            if(s>mx):
+                mx=s
+            if(s<0):
+                s=0
+        return mx
+
+        
