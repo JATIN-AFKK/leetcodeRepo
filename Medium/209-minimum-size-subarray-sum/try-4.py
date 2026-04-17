01be6ccb357d7f59abea957f85de8366d2ce1201
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 20:57:12 +0530

    Add Medium/209-minimum-size-subarray-sum/try-4.py

---
+++ Medium/209-minimum-size-subarray-sum/try-4.py
@@ -0,0 +1,23 @@
+/*
+ * Problem #209: Minimum Size Subarray Sum
+ * Difficulty: Medium
+ * Submission: Try 4
+ * status: Accepted
+ * Language: python3
+ * Date: 08/12/2024, 23:40:41
+ * Link: https://leetcode.com/problems/minimum-size-subarray-sum/
+ */
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        start=0;s=0;mx=float('inf')
+        for e in range(len(nums)):
+            s=s+nums[e]
+            while(s>=target):
+                if (e-start+1<mx):
+                    mx=e-start+1
+                
+                s-=nums[start]
+                start+=1
+        return 0 if mx==float('inf') else mx
+
