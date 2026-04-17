adb8426e0b349125d1234385d14aa4c622b484df
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 21:07:16 +0530

    Add Easy/14-longest-common-prefix/try-1.cpp

---
+++ Easy/14-longest-common-prefix/try-1.cpp
@@ -0,0 +1,29 @@
+/*
+ * Problem #14: Longest Common Prefix
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 15/11/2024, 13:03:31
+ * Link: https://leetcode.com/problems/longest-common-prefix/
+ */
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string s="";
+        int k=0,i;
+        while(true){
+            for(i=0;i<strs.size();i++){
+                if(k>=strs[i].size()){
+                    return s;
+                }
+                else if(strs[0][k]!=strs[i][k]){
+                    return s;
+                }
+            }
+            s+=strs[i-1][k];
+            k++;
+        }
+    }
+};
