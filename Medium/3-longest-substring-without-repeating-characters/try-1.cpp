1d63b791319793ff6c8779f85131deaeed3ec1ef
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 21:05:44 +0530

    Add Medium/3-longest-substring-without-repeating-characters/try-1.cpp

---
+++ Medium/3-longest-substring-without-repeating-characters/try-1.cpp
@@ -0,0 +1,37 @@
+/*
+ * Problem #3: Longest Substring Without Repeating Characters
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 22/11/2024, 23:07:45
+ * Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ */
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char,char> map;
+        string r="";
+        int c=0,sum=0;
+        for(int i=0;i<s.size();i++){
+            r="";
+            c=0;
+            map.clear();
+            for(int j=i;j<s.size();j++){
+                r+=s[j];
+                if(map[s[j]]>0){
+                    break;
+                }
+                else{
+                    map[s[j]]++;
+                    c++;
+                }
+            }
+            if(c>sum){
+                sum=c;
+            }
+        }
+        return sum;
+    }
+};
