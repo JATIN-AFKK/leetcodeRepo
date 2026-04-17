40ee36167894f287664b06ac489505e155d46c37
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 21:01:24 +0530

    Add Easy/242-valid-anagram/try-19.py

---
+++ Easy/242-valid-anagram/try-19.py
@@ -0,0 +1,34 @@
+/*
+ * Problem #242: Valid Anagram
+ * Difficulty: Easy
+ * Submission: Try 19
+ * status: Accepted
+ * Language: python3
+ * Date: 06/12/2024, 16:14:15
+ * Link: https://leetcode.com/problems/valid-anagram/
+ */
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        d={}
+        for i in s:
+            if i not in d:
+                d[i]=1
+            else:
+                d[i]+=1
+        
+        for i in t:
+            if i not in d:
+                return False
+            else:
+                d[i]-=1
+                if d[i]<0:
+                    return False
+            
+        for i in d:
+            if d[i]!=0 :
+                return False
+
+        return True
+        
+        
