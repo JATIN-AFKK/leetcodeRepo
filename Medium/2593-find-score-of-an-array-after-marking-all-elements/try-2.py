6563feb5e7037167323d2ae0499042bee51166cf
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 20:54:44 +0530

    Add Medium/2593-find-score-of-an-array-after-marking-all-elements/try-2.py

---
+++ Medium/2593-find-score-of-an-array-after-marking-all-elements/try-2.py
@@ -0,0 +1,43 @@
+/*
+ * Problem #2593: Find Score of an Array After Marking All Elements
+ * Difficulty: Medium
+ * Submission: Try 2
+ * status: Accepted
+ * Language: python3
+ * Date: 13/12/2024, 07:25:11
+ * Link: https://leetcode.com/problems/find-score-of-an-array-after-marking-all-elements/
+ */
+
+class Solution:
+    def findScore(self, nums: List[int]) -> int:
+        heap=[(val,ind) for ind,val in enumerate(nums)]
+        visit=set()
+        heapify(heap)
+        s=0
+        n=len(nums)
+        while heap:
+            val,ind=heappop(heap)
+            if ind in visit:
+                continue
+            
+            s=s+val
+            visit.add(ind)
+            if (ind>0):
+                visit.add(ind-1)
+            if (ind<n-1):
+                visit.add(ind+1)
+
+
+        return s
+        
+
+        
+
+
+
+        
+
+
+
+
+        
