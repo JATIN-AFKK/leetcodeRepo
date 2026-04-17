173996958d29165b67d21dbc66f85a583ff2992d
JATIN-AFKK <jg9257694@gmail.com>
2026-04-17 21:06:57 +0530

    Add Medium/223-rectangle-area/try-2.cpp

---
+++ Medium/223-rectangle-area/try-2.cpp
@@ -0,0 +1,17 @@
+/*
+ * Problem #223: Rectangle Area
+ * Difficulty: Medium
+ * Submission: Try 2
+ * status: Accepted
+ * Language: cpp
+ * Date: 21/11/2024, 18:11:14
+ * Link: https://leetcode.com/problems/rectangle-area/
+ */
+
+class Solution {
+public:
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2){
+        
+        return (ax2-ax1)*(ay2-ay1)+(bx2-bx1)*(by2-by1)-max(0,min(ax2,bx2)-max(ax1,bx1))*max(0,min(ay2,by2)-max(ay1,by1));
+    } 
+};
